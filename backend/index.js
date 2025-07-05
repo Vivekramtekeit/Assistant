6526e08f538c73453328d559f7fdd9e803c4fea0
Vivekramtekeit <viveknitrr26@gmail.com>
2025-07-06 03:03:03 +0530

    final commit of project

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,4 @@
-// import express from "express"
-// import dotenv from "dotenv"
-// dotenv.config()
-// import connectDb from "./config/db.js"
-// import authRouter from "./routes/auth.routes.js"
-// import cors from "cors"
-// import cookieParser from "cookie-parser"
-// import userRouter from "./routes/user.routes.js"
-// import geminiResponse from "./gemini.js"
 
-
-// const app=express()
-// app.use(cors({
-//     origin:"http://localhost:5173",
-//     credentials:true
-// }))
-// const port=process.env.PORT || 5000
-// app.use(express.json())
-// app.use(cookieParser())
-// app.use("/api/auth",authRouter)
-// app.use("/api/user",userRouter)
-
-
-// app.listen(port,()=>{
-//     connectDb()
-//     console.log("server started")
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ✅ backend/index.js
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
@@ -51,37 +11,6 @@ import userRouter from "./routes/user.routes.js"
 const app = express()
 
 console.log("NODE_ENV:", process.env.NODE_ENV);
-
-
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true
-// }))
-
-
-
-
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://frontendassistant-h4gd.onrender.com"
-// ];
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true
-// }));
-
-
-
-
-
-
 
 
 
@@ -99,12 +28,6 @@ app.use(cors({
   },
   credentials: true
 }));
-
-
-
-
-
-
 
 
 
